078da87d6b2f7027481fe33597d4d7c2fec1328d
JA <joshappeldev@gmail.com>
2019-04-15 23:04:24 -0400

    modal positioning and modal scrolls

--- client/src/containers/Main.js
+++ client/src/containers/Main.js
@@ -951,73 +951,77 @@ class Main extends Component {
             </div>
           </div>
           <div>
-            <Modal
-              style={{ marginTop: 100 }}
-              isOpen={this.state.modal}
-              toggle={this.toggle}
-              className={this.props.className}
-            >
-              <ModalHeader toggle={this.toggle}>Walmart Results</ModalHeader>
-              <ModalBody>
-                <div className="row align-items-center">
-                  {this.state.itemImages.length === 0 ? (
-                    <React.Fragment>
-                      <h3 className="p-3 text-center">Product Results</h3>
-                      <p className="p-3 wait-msg">
-                        If search items don't show up quickly, Close this modal
-                        and try again.
-                      </p>
-                    </React.Fragment>
-                  ) : (
-                    this.state.itemImages.map((item, index) => {
-                      return (
-                        <div
-                          key={index}
-                          className="col-12 col-md-12 text-center"
-                        >
-                          <img
-                            src={item.largeImage}
-                            alt={item}
-                            className="img-fluid rounded border border-dark my-2"
-                          />
-                          <p className="itemName font-weight-bold">
-                            {item.name}
-                          </p>
-                          <p className="itemPrice font-weight-bold">
-                            ${item.salePrice}
-                          </p>
-                          <a
-                            target="_blank"
-                            href={item.productUrl}
-                            rel="noopener noreferrer"
-                            className="m-2 text-center btn btn-outline-dark"
+            <Grid container justify="center">
+              <Modal
+                style={{ marginTop: 100 }}
+                isOpen={this.state.modal}
+                toggle={this.toggle}
+                className={this.props.className}
+                scrollable
+                id="modalContainer"
+              >
+                <ModalHeader toggle={this.toggle}>Walmart Results</ModalHeader>
+                <ModalBody className="scrollModal">
+                  <div className="row align-items-center">
+                    {this.state.itemImages.length === 0 ? (
+                      <React.Fragment>
+                        <h3 className="p-3 text-center">Product Results</h3>
+                        <p className="p-3 wait-msg">
+                          If search items don't show up quickly, Close this
+                          modal and try again.
+                        </p>
+                      </React.Fragment>
+                    ) : (
+                      this.state.itemImages.map((item, index) => {
+                        return (
+                          <div
+                            key={index}
+                            className="col-12 col-md-12 text-center"
                           >
-                            View on Walmart.com
-                          </a>
-                          <button
-                            className="m-2 text-center btn btn-dark"
-                            name={item.name}
-                            value={item.salePrice}
-                            onClick={this.handleWalmartSubmit}
-                          >
-                            Add to Budget
-                          </button>
-                        </div>
-                      );
-                    })
-                  )}
-                </div>
-              </ModalBody>
-              <ModalFooter>
-                <Button
-                  variant="contained"
-                  className="button"
-                  onClick={this.toggle}
-                >
-                  Close
-                </Button>
-              </ModalFooter>
-            </Modal>
+                            <img
+                              src={item.largeImage}
+                              alt={item}
+                              className="img-fluid rounded border border-dark my-2 productImg"
+                            />
+                            <p className="itemName font-weight-bold">
+                              {item.name}
+                            </p>
+                            <p className="itemPrice font-weight-bold">
+                              ${item.salePrice}
+                            </p>
+                            <a
+                              target="_blank"
+                              href={item.productUrl}
+                              rel="noopener noreferrer"
+                              className="m-2 text-center btn btn-outline-dark"
+                            >
+                              View on Walmart.com
+                            </a>
+                            <button
+                              className="m-2 text-center btn btn-dark"
+                              name={item.name}
+                              value={item.salePrice}
+                              onClick={this.handleWalmartSubmit}
+                            >
+                              Add to Budget
+                            </button>
+                          </div>
+                        );
+                      })
+                    )}
+                  </div>
+                </ModalBody>
+                <ModalFooter>
+                  <Button
+                    variant="contained"
+                    className="button"
+                    onClick={this.toggle}
+                  >
+                    Close
+                  </Button>
+                </ModalFooter>
+              </Modal>
+            </Grid>
           </div>
           <br />
           <br />
