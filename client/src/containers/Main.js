cf94a7497d417c2a9d65107dda8231bffa80f75a
JA <joshappeldev@gmail.com>
2019-04-16 23:54:09 -0400

    removed unused imports

--- client/src/containers/Main.js
+++ client/src/containers/Main.js
@@ -16,8 +16,6 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import MenuIcon from "@material-ui/icons/Menu";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
 import Grid from "@material-ui/core/Grid";
 import IconButton from "@material-ui/core/IconButton";
 import Button from "@material-ui/core/Button";
