9cd8521819b4bba10dafc20d59d0b7305a758210
JA <joshappeldev@gmail.com>
2019-04-21 11:28:24 -0400

    extracted welcome component

--- client/src/components/Charts.jsx
+++ client/src/components/Charts.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import moment from "moment";
+import Welcome from "./Welcome";
 import { Chart } from "primereact/chart";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
@@ -13,7 +14,6 @@ const Charts = ({
   falseIncome,
   pieChart,
   arrayForCatByCurrentMonth,
-  budgetTotal,
   arrayForBudgetTable
 }) => {
   const month = moment().format("MMMM");
@@ -154,45 +154,7 @@ const Charts = ({
   };
 
   return arrayForBudgetTable.length === 0 ? (
-    <div className="row justify-content-center">
-      <div className="col-12 col-md-8">
-        <Grid container justify="center">
-          <Card className="welcomeCard">
-            <CardContent>
-              <div className="welcomeWrapper">
-                <h2 className="text-center welcomeTitle">WELCOME!</h2>
-                <hr />
-                <div className="welcomeMsgWrapper">
-                  <p className="welcomeMsg">
-                    Add some items to your budget to get started. You can add
-                    budget income or expenses as individual transactions or
-                    recurring transactions up to 6 months forward. You can also
-                    search Walmart.com to find products and add them to your
-                    budget or view them on Walmart.com.
-                  </p>
-                  <hr />
-                  <p className="welcomeMsg">
-                    After you add a transaction this message will disappear and
-                    charts will replace it like magic! Once you add income, your
-                    time based charts will populate. Once you add an expense
-                    your spending based charts will populate.
-                  </p>
-                  <hr />
-                  <p className="welcomeMsg">
-                    The table above can be used to delete budget items
-                    individually or to sort by relevant the column headers. The
-                    budget total at the top will represent your disposable
-                    income after you add both income and expenses to your
-                    budget. If you only track income or only track expenses it
-                    will represent the sums of your total budget items.
-                  </p>
-                </div>
-              </div>
-            </CardContent>
-          </Card>
-        </Grid>
-      </div>
-    </div>
+    <Welcome />
   ) : (
     <div className="row justify-content-center">
       <div className="col-12">
