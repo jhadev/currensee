d0cad55cb2bc290b68fb4c8cecd3f70373d59a19
JA <joshappeldev@gmail.com>
2019-05-04 01:09:45 -0400

    refactoring

--- client/src/components/WalmartSearch.jsx
+++ client/src/components/WalmartSearch.jsx
@@ -4,41 +4,43 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 
 const WalmartSearch = ({ itemToSearch, handleInputChange, handleSearch }) => {
-  return (
+  const Wrapper = props => (
     <div className="row justify-content-center">
       <div className="col-md-6 col-12">
-        {/* <Grid container justify="center"> */}
         <Card className="total-sum">
-          <CardContent>
-            <div className="text-center text-primary">
-              <img
-                src="https://user-images.githubusercontent.com/42519030/56179504-f5e9f700-5fd3-11e9-9d89-ad7ee7800272.png"
-                alt="walmart"
-                className="img-fluid walmartLogo mb-2"
-              />
-            </div>
-            <input
-              className="form-control"
-              value={itemToSearch}
-              onChange={handleInputChange}
-              name="itemToSearch"
-              placeholder="Search for a product"
-              type="text"
-              list="item-list"
-            />
-            <Button
-              variant="contained"
-              color="secondary"
-              className="btn text-center btn-outline-primary my-2"
-              onClick={handleSearch}
-            >
-              Search
-            </Button>
-          </CardContent>
+          <CardContent>{props.children}</CardContent>
         </Card>
-        {/* </Grid> */}
       </div>
     </div>
+  );
+
+  return (
+    <Wrapper>
+      <div className="text-center text-primary">
+        <img
+          src="https://user-images.githubusercontent.com/42519030/56179504-f5e9f700-5fd3-11e9-9d89-ad7ee7800272.png"
+          alt="walmart"
+          className="img-fluid walmartLogo mb-2"
+        />
+      </div>
+      <input
+        className="form-control"
+        value={itemToSearch}
+        onChange={handleInputChange}
+        name="itemToSearch"
+        placeholder="Search for a product"
+        type="text"
+        list="item-list"
+      />
+      <Button
+        variant="contained"
+        color="secondary"
+        className="btn text-center btn-outline-primary my-2"
+        onClick={handleSearch}
+      >
+        Search
+      </Button>
+    </Wrapper>
   );
 };
 
