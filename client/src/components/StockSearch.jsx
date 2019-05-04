ad755ea6aa94c4be357a8e80db699e3b6840f947
JA <joshappeldev@gmail.com>
2019-05-04 17:08:10 -0400

    fix

--- client/src/components/StockSearch.jsx
+++ client/src/components/StockSearch.jsx
@@ -10,73 +10,70 @@ const StockSearch = ({
   handleStockSearch,
   stockToSend
 }) => {
-  const Wrapper = props => (
-    <div className="col-md-6 col-12">
-      <Card className="stockSearch mb-3">
-        <CardContent>{props.children}</CardContent>
-      </Card>
-    </div>
-  );
   return (
     <div className="row justify-content-center">
-      <Wrapper>
-        <div className="text-center">
-          <img
-            src="https://user-images.githubusercontent.com/42519030/56464903-1691c280-63c1-11e9-8c35-f067abeda506.png"
-            alt="tradingview"
-            className="img-fluid walmartLogo text-center my-4"
-          />
-        </div>
-        <input
-          className="form-control"
-          value={stockToSearch}
-          onChange={handleInputChange}
-          name="stockToSearch"
-          placeholder="Enter a symbol, ex: AAPL, BTC"
-          type="text"
-          list="item-list"
-        />
-        {stockToSend === "" ? (
-          <Button
-            variant="contained"
-            color="secondary"
-            className="btn text-center btn-outline-primary my-2"
-            onClick={handleStockSearch}
-          >
-            Search
-          </Button>
-        ) : stockToSearch.length === 0 ? (
-          <div>
-            <Button
-              variant="contained"
-              color="secondary"
-              className="text-center my-2"
-              onClick={handleStockSearch}
-            >
-              Clear Chart
-            </Button>
-            <Button
-              variant="contained"
-              color="primary"
-              className="text-center yahooLink m-2"
-              target="_blank"
-              href={`https://finance.yahoo.com/quote/${stockToSend.toUpperCase()}?p=${stockToSend.toUpperCase()}&.tsrc=fin-srch`}
-              rel="noopener noreferrer"
-            >
-              View on Yahoo! Finance
-            </Button>
-          </div>
-        ) : (
-          <Button
-            variant="contained"
-            color="secondary"
-            className="text-center my-2"
-            onClick={handleStockSearch}
-          >
-            Search Again
-          </Button>
-        )}
-      </Wrapper>
+      <div className="col-md-6 col-12">
+        <Card className="stockSearch mb-3">
+          <CardContent>
+            <div className="text-center">
+              <img
+                src="https://user-images.githubusercontent.com/42519030/56464903-1691c280-63c1-11e9-8c35-f067abeda506.png"
+                alt="tradingview"
+                className="img-fluid walmartLogo text-center my-4"
+              />
+            </div>
+            <input
+              className="form-control"
+              value={stockToSearch}
+              onChange={handleInputChange}
+              name="stockToSearch"
+              placeholder="Enter a symbol, ex: AAPL, BTC"
+              type="text"
+              list="item-list"
+            />
+            {stockToSend === "" ? (
+              <Button
+                variant="contained"
+                color="secondary"
+                className="btn text-center btn-outline-primary my-2"
+                onClick={handleStockSearch}
+              >
+                Search
+              </Button>
+            ) : stockToSearch.length === 0 ? (
+              <div>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  className="text-center my-2"
+                  onClick={handleStockSearch}
+                >
+                  Clear Chart
+                </Button>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className="text-center yahooLink m-2"
+                  target="_blank"
+                  href={`https://finance.yahoo.com/quote/${stockToSend.toUpperCase()}?p=${stockToSend.toUpperCase()}&.tsrc=fin-srch`}
+                  rel="noopener noreferrer"
+                >
+                  View on Yahoo! Finance
+                </Button>
+              </div>
+            ) : (
+              <Button
+                variant="contained"
+                color="secondary"
+                className="text-center my-2"
+                onClick={handleStockSearch}
+              >
+                Search Again
+              </Button>
+            )}
+          </CardContent>
+        </Card>
+      </div>
       <div className="col-md-8 col-12">
         {stockToSend !== "" && (
           <Card className="widgetCard">
--- client/src/components/WalmartSearch.jsx
+++ client/src/components/WalmartSearch.jsx
@@ -4,43 +4,39 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 
 const WalmartSearch = ({ itemToSearch, handleInputChange, handleSearch }) => {
-  const Wrapper = props => (
+  return (
     <div className="row justify-content-center">
       <div className="col-md-6 col-12">
         <Card className="total-sum">
-          <CardContent>{props.children}</CardContent>
+          <CardContent>
+            <div className="text-center text-primary">
+              <img
+                src="https://user-images.githubusercontent.com/42519030/56179504-f5e9f700-5fd3-11e9-9d89-ad7ee7800272.png"
+                alt="walmart"
+                className="img-fluid walmartLogo mb-2"
+              />
+            </div>
+            <input
+              className="form-control"
+              value={itemToSearch}
+              onChange={handleInputChange}
+              name="itemToSearch"
+              placeholder="Search for a product"
+              type="text"
+              list="item-list"
+            />
+            <Button
+              variant="contained"
+              color="secondary"
+              className="btn text-center btn-outline-primary my-2"
+              onClick={handleSearch}
+            >
+              Search
+            </Button>
+          </CardContent>
         </Card>
       </div>
     </div>
-  );
-
-  return (
-    <Wrapper>
-      <div className="text-center text-primary">
-        <img
-          src="https://user-images.githubusercontent.com/42519030/56179504-f5e9f700-5fd3-11e9-9d89-ad7ee7800272.png"
-          alt="walmart"
-          className="img-fluid walmartLogo mb-2"
-        />
-      </div>
-      <input
-        className="form-control"
-        value={itemToSearch}
-        onChange={handleInputChange}
-        name="itemToSearch"
-        placeholder="Search for a product"
-        type="text"
-        list="item-list"
-      />
-      <Button
-        variant="contained"
-        color="secondary"
-        className="btn text-center btn-outline-primary my-2"
-        onClick={handleSearch}
-      >
-        Search
-      </Button>
-    </Wrapper>
   );
 };
 
